88b6c64017f652a8fd4a8005e076aa9ddc58ef7c
Richard Young <richardyoung@Richards-MacBook-Air.local>
2022-10-12 16:16:33 -0400

    created two files

---
+++ Exercise_1/index.js
@@ -0,0 +1,15 @@
+// const button = document.querySelector("button");
+// button.addEventListener("click", function () {
+//   console.log("Hello world!");
+// });
+
+let total = 0
+
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  total = total + 1;
+  console.log(total)
+} )
+
+
+
